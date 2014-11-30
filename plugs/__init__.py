c6348ea880e14d59b12ac0643eb1d3e7e2d4c32e
Eugene Eeo <xe0jun@hotmail.com>
2014-11-30 17:05:30 +0800

    doctsrings and license

--- plugs/__init__.py
+++ plugs/__init__.py
@@ -1,1 +1,13 @@
+"""
+    plugs
+    ~~~~~
+    Plugs is a library that implements a Plug object,
+    which is a simple, stateful wrapper around the
+    lower-level ``select.select`` function.
+
+    :copyright: (c) 2014 Eugene Eeo
+    :license: MIT, see LICENSE for details.
+"""
+
+
 from plugs.wrapper import Plug
