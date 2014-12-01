744792ba55582c75dee869cfd0fc5ba18b49f5fc
Eugene Eeo <xe0jun@hotmail.com>
2014-12-01 21:07:19 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='leafman',
+    version='0.0.1',
+    description='simple wrapper atop select',
+    license='MIT',
+    author='Eugene Eeo',
+    author_email='packwolf58@gmail.com',
+    url='https://github.com/eugene-eeo/plugs',
+    packages=['plugs'],
+
+    platforms='any',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
