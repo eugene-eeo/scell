e7c08a9757bd7a8c948d04a32a63d98f122b7401
Eugene Eeo <xe0jun@hotmail.com>
2014-12-06 10:54:17 +0800

    update setup.py [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scell',
-    version='0.0.2',
+    version='0.1.0',
     description='simple wrapper atop select',
     license='MIT',
     author='Eugene Eeo',
