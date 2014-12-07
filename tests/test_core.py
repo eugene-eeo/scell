71399086be980b4efe2114fb09b414db1012fc42
Eugene Eeo <xe0jun@hotmail.com>
2014-12-07 14:08:39 +0800

    test out all possibilities for Monitored.ready

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,3 +1,5 @@
+from itertools import chain
+from pytest import mark
 from scell.core import select, Monitored
 
 
@@ -12,9 +14,21 @@ def test_monitored(handles, mode):
         assert monitor.mode == mode
         assert not monitor.callback()
 
-        assert not monitor.ready
 
-        monitor.readable = True
-        monitor.writable = True
+@mark.parametrize(
+    'fmode,attrs',
+    chain(
+        [mark.xfail(('rw', m)) for m in [(0,1), (1,0), (0,0)]],
+        [('w', m) for m in [(0,1), (1,1)]],
+        [('r', m) for m in [(1,0), (1,1)]],
+        [('rw', (1,1))],
+    )
+)
+def test_monitored_ready(handles, fmode, attrs):
+    for monitor in [Monitored(fp, fmode) for fp in handles]:
+        r, w = attrs
+        monitor.readable = r
+        monitor.writable = w
 
         assert monitor.ready
+        assert monitor.mode == fmode
