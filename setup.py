1bd7f7d493f968e73a205232f793e521daf8d805
Eugene Eeo <xe0jun@hotmail.com>
2014-12-07 10:19:36 +0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='scell',
-    version='0.1.0',
+    version='0.1.1',
     description='simple wrapper atop select',
     license='MIT',
     author='Eugene Eeo',
