c4855670222c1172b3dfdfff8a787ff90ee2012d
Eugene Eeo <xe0jun@hotmail.com>
2014-12-08 20:29:55 +0800

    no need for tmpdir

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,13 @@ def mode(request):
 
 
 @fixture
-def handle(request, tmpdir):
-    fp = tmpdir.join('file')
-    return fp.open(mode='w+')
+def handle(request):
+    return stdout
 
 
-@fixture(params=['stdio', 'files'])
-def handles(request, tmpdir):
-    if request.param == 'stdio':
-        return [stdout, stderr]
-
-    paths = [tmpdir.join(x) for x in ['file1', 'file2']]
-    return [fp.open(mode='w+') for fp in paths]
+@fixture
+def handles(request):
+    return [stdout, stderr]
 
 
 @fixture
