f5568064510c8a439a06740845a9630b7c07ff00
Eugene Eeo <xe0jun@hotmail.com>
2014-12-12 22:55:55 +0800

    added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from distutils.core import setup
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['--strict', '--verbose', '--tb=long', 'tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
 
 setup(
     name='scell',
@@ -10,6 +25,12 @@ setup(
     url='https://github.com/eugene-eeo/scell',
     keywords="selector io reactor",
     packages=['scell'],
+
+    cmdclass={'test': PyTest},
+    tests_require=['pytest'],
+    extras_require={
+        'testing': ['pytest'],
+    },
 
     platforms='any',
     classifiers=[
