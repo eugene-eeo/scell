d0f29a583c43e4b06dd0ba510f108a05107caca2
Eugene Eeo <xe0jun@hotmail.com>
2014-12-12 17:27:13 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='Eugene Eeo',
     author_email='packwolf58@gmail.com',
     url='https://github.com/eugene-eeo/scell',
+    keywords="selector io reactor",
     packages=['scell'],
 
     platforms='any',
