471449607bda183eab4b2ffea170101f285f368e
Eugene Eeo <xe0jun@hotmail.com>
2015-01-02 09:42:14 +0800

    happy new year!

--- scell/__init__.py
+++ scell/__init__.py
@@ -5,7 +5,7 @@
     Library providing a simple, stateful wrapper
     around the low-level ``~select.select`` API.
 
-    :copyright: (c) 2014 Eugene Eeo
+    :copyright: (c) 2014-2015 Eugene Eeo
     :license: MIT, see LICENSE for details.
 """
 
