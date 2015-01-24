0559e812a4eb456d37bf28cb4eb516624e322bd0
Eugene Eeo <xe0jun@hotmail.com>
2015-01-24 22:52:23 +0800

    performance improvements

--- scell/wrapper.py
+++ scell/wrapper.py
@@ -50,7 +50,7 @@ class Selector(dict):
         ideal for use where you might modify the
         selector while iterating.
         """
-        return [fp for fp in self]
+        return list(self)
 
     @property
     def rwlist(self):
