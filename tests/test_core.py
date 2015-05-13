31213a8edbeba95c79d7950ac06eb534dd3d48e8
Eugene Eeo <packwolf58@gmail.com>
2015-05-13 18:52:01 +0800

    add tests for events

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,5 +1,5 @@
-from pytest import mark
-from scell.core import select, Monitored
+from pytest import mark, fixture
+from scell.core import select, Monitored, Event
 
 
 def test_select(handles):
@@ -8,6 +8,23 @@ def test_select(handles):
     assert select(handles, handles) == (handles, handles)
 
 
-def test_monitored_default_callback():
-    monitor = Monitored(None, None, None)
-    assert monitor.callback() is None
+@fixture
+def monitored():
+    return Monitored(None,
+                     wants_read=True,
+                     wants_write=True)
+
+
+def test_monitored_default_callback(monitored):
+    assert monitored.callback() is None
+
+
+@mark.parametrize('changes,ok', [
+    ((1,0), False),
+    ((0,1), False),
+    ((0,0), False),
+    ((1,1), True),
+])
+def test_event_ready(monitored, changes, ok):
+    event = Event(monitored, *changes)
+    assert event.ready == ok
