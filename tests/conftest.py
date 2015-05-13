58b08aab90f001b435ccaac1519e3bf965f88e92
Eugene Eeo <packwolf58@gmail.com>
2015-05-13 21:59:18 +0800

    avoid select.select on windows when testing
    this is because select.select doesn't work on files
    in Windows but only sockets and it is very hairy to
    test sockets.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
+import os
 from sys import stdout, stderr
 from scell import Selector
 from pytest import fixture
+
+
+if os.name == 'nt':
+    @fixture(autouse=True)
+    def select_no_io(monkeypatch):
+        monkeypatch.setattr(
+            'select.select',
+            lambda rl, wl, xl, timeout=None: (rl, wl, xl),
+        )
 
 
 @fixture
